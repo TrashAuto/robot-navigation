fe03426684900eb20007add86f42ce2b78424db5
zzaid17 <91937100+zzaid17@users.noreply.github.com>
2025-03-25 08:55:16 -0600

    bno055 code v1

---
+++ bno055.py
@@ -0,0 +1,43 @@
+import time
+import board
+import adafruit_bno055
+
+i2c = board.I2C()
+sensor = adafruit_bno055.BNO055_I2C(i2c)
+
+# Initialize state variables
+velocity = [0.0, 0.0, 0.0]   # x, y, z velocity
+position = [0.0, 0.0, 0.0]   # x, y, z position
+angle = [0.0, 0.0, 0.0]      # x, y, z angle (degrees)
+
+prev_time = time.time()
+
+# Integrate acceleration for distance
+def integrate_accel(acc, vel, pos, dt):
+    vel_new = [vel[i] + acc[i] * dt for i in range(3)]
+    pos_new = [pos[i] + vel_new[i] * dt for i in range(3)]
+    return vel_new, pos_new
+
+# Integrate angular velocity for angle
+def integrate_gyro(gyro, angle, dt):
+    return [angle[i] + (gyro[i] * dt * 180 / 3.14159) for i in range(3)]
+
+print("Outputting distance (m) and angle (deg):\n")
+
+while True:
+    curr_time = time.time()
+    dt = curr_time - prev_time
+    prev_time = curr_time
+
+    acc = sensor.linear_acceleration
+    gyro = sensor.gyro
+
+    if acc is not None and gyro is not None:
+        velocity, position = integrate_accel(acc, velocity, position, dt)
+        angle = integrate_gyro(gyro, angle, dt)
+
+        print(f"Distance (m): {[round(p, 2) for p in position]}")
+        print(f"Angle (deg): {[round(a, 2) for a in angle]}")
+        print()
+
+    time.sleep(0.1)
