3fe50762891f2202d50feca54c7c3e358025199c
zzaid17 <91937100+zzaid17@users.noreply.github.com>
2025-04-04 17:26:17 -0600

    perimeter fixes & lidar to garbage collection distance

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ from classification import run_ml_pipeline
 from perception import detect_object_of_interest, is_tall_object_present
 from navigation import start_path_distance, update_path_distance, reset_path_distance
 
-# Declare parameters
-PERIMETER_X = 500 # cm (5m^2) area
+# Declare parameters (all distances in cm)
+PERIMETER_X = 750
 PERIMETER_Y = 500
 observed_distance = 50
 observed_angle = math.radians(45)
 side_distance = math.cos(observed_angle) * observed_distance / math.sin(observed_angle)
+collection_distance = 10 # Distance between garbage and LiDAR when collecting garbage
 
 def loop(PERIMETER_X, PERIMETER_Y):
     start_path_distance("y")
@@ -44,16 +45,16 @@ def loop(PERIMETER_X, PERIMETER_Y):
         distance_travelled_y = update_path_distance("y")
         distance_travelled_x = update_path_distance("x")
 
-        if distance_travelled_y >= 0.8 * PERIMETER_Y:
+        if distance_travelled_y >= 0.9 * PERIMETER_Y:
             turn_right_until(90, "path")
             move_forward_until(side_distance * 1.5, "path", "x")
             turn_right_until(90, "path")
             reset_path_distance("y")
             start_path_distance("y")
 
-        if distance_travelled_x >= 0.8 * PERIMETER_X:
+        if distance_travelled_x >= 0.9 * PERIMETER_X:
             turn_right_until(90, "path")
-            move_forward_until(0.8 * PERIMETER_X, "path", "y")
+            move_forward_until(0.9 * PERIMETER_X, "path", "y")
             turn_right_until(90, "path")
             reset_path_distance("x")
             start_path_distance("x")
@@ -66,12 +67,12 @@ def object_event_off_path(object_distance, object_angle):
     else: 
         turn_right_until(object_angle, "object")
     
-    move_forward_until(object_distance, "object")
+    move_forward_until(object_distance - collection_distance, "object")
     
     if not is_tall_object_present(object_distance * 1000) and run_ml_pipeline():
         collect_garbage()
         
-    move_backward_until(object_distance, "object")
+    move_backward_until(object_distance - collection_distance, "object")
     
     if object_angle < 0:
         turn_right_until(-object_angle, "object")
@@ -79,7 +80,7 @@ def object_event_off_path(object_distance, object_angle):
         turn_left_until(object_angle, "object")
         
 def object_event_on_path(object_distance, object_width):
-    move_forward_until(object_distance, "object")
+    move_forward_until(object_distance - collection_distance, "object")
     if not is_tall_object_present(object_distance * 1000) and run_ml_pipeline():
         collect_garbage()
     else:
@@ -88,11 +89,11 @@ def object_event_on_path(object_distance, object_width):
     
 def obstacle_event(object_width):
     turn_right_until(90, "object")
-    move_forward_until(2 * object_width)
+    move_forward_until(3 * object_width)
     turn_left_until(90, "object")
-    move_forward_until(2 * object_width)
+    move_forward_until(3 * object_width)
     turn_left_until(90, "object")
-    move_forward_until(2 * object_width)
+    move_forward_until(3 * object_width)
     turn_right_until(90, "object")
 
 if __name__ == "__main__":
